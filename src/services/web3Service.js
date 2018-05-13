8b066f22da81d2031d9a0121ffadc72be332e3a1
Alex Bazhanau <bazhanau@outlook.com>
2018-05-13 11:19:57 -0400

    removed accounts[0] check

--- src/services/web3Service.js
+++ src/services/web3Service.js
@@ -1,36 +1,14 @@
 import Web3 from 'web3';
 
-if (typeof window.web3 !== 'undefined') {
-  let defaultAccount = window.web3.eth.accounts[0];
-  if (!defaultAccount) {
-    alert('Please, log in Metamask, choose Kovan network and reload the page.');
+window.addEventListener('load', function () {
+  if (typeof window.web3 !== 'undefined') {
+    window.web3 = new Web3(window.web3.currentProvider);
+  } else {
+    alert('Please, install metamask.io extension in your browser.');
   }
-  window.web3 = new Web3(window.web3.currentProvider);
-  window.web3.eth.defaultAccount = defaultAccount;
-} else {
-  alert('Please, install metamask.io extension in your browser.');
-}
+})
 
 export default window.web3 && window.web3.currentProvider;
-
-export function getWalletBalanceAsync() {
-  return new Promise((resolve, reject) => {
-    const account = getDefaultAccount()
-    if (!account) {
-      resolve(0);
-      return;
-    }
-
-    window.web3.eth.getBalance(getDefaultAccount(), (err, balance) => {
-      if (err) {
-        reject(err);
-      }
-      else {
-        resolve(window.web3.fromWei(balance));
-      }
-    });
-  });
-}
 
 export function getNetwork() {
   return window.web3.version.network;
@@ -45,8 +23,4 @@ export function getNetworkAsync() {
       resolve(netId);
     });
   });
-}
-
-export function getDefaultAccount() {
-  return window.web3 && window.web3.eth.accounts[0];
 }
