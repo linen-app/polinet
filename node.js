b7f7b3a2def628eca9417edec07a648665d0bc60
Alex Bazhanau <bazhanau@outlook.com>
2018-05-19 00:01:51 -0400

    orders validation for node.js version

--- node.js
+++ node.js
@@ -1,14 +1,26 @@
 const pubsubService = require('./src/services/pubsubService.js');
 const dharmaService = require('./src/services/dharmaService.js');
 
-let orders = {};
+let _orders = {};
 
 pubsubService.subscribe(
     (orderJson) => dharmaService
         .parseJsonOrder(orderJson)
         .then(order => {
-            orders = Object.assign({ [order.hash]: order }, orders);
+            _orders = Object.assign({ [order.hash]: order }, _orders);
             console.log(`New order recieved: ${JSON.stringify(order)}`);
         }),
-    () => Object.values(orders)
+    () => Object.values(_orders)
 );
+
+setInterval(() => {
+    const ordersArray = Object.values(_orders);
+    const orderPromises = ordersArray.map(order => dharmaService.validateOrderAsync(order).catch(() => null));
+    Promise.all(orderPromises)
+        .then(orders => orders.filter(x => x))
+        .then(orders => orders.reduce((prev, curr) => Object.assign({ [curr.hash]: curr }, prev), {}))
+        .then(orders => {
+            console.log(`Filtered orders length: ${Object.keys(orders).length}`)
+            _orders = orders;
+        });
+}, 3000)
